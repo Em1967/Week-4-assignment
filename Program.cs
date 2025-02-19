0646c84aec643f5296c1363acd02d6d089efeda4
Em1967 <ElijahMichel2468@icloud.com>
2025-02-19 14:05:33 -0600

    [1.05] logger/dk.csv file confirmation

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
 ﻿
+﻿using NLog;
+string path = Directory.GetCurrentDirectory() + "//nlog.config";
+
+// create instance of Logger
+var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
+
+logger.Info("Program started");
+
+string file = "dk.csv";
+// make sure movie file exists
+if (!File.Exists(file))
+{
+    logger.Error("File does not exist: {File}", file);
+}
+else
+{
+    // TODO: create user menu
+}
+
+logger.Info("Program ended");
